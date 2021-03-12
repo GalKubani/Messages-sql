c5bb0d63a5b1b4c83edc9cf7f78ba59045eb62d6
gal kubani <galkubani@gmail.com>
2021-03-12 19:09:00 +0200

    adding route to home

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(cors());
 app.use(messageRouter);
 
+app.use("/",(req,res)=>{
+    res.send("ok")
+})
+
 sequelize.sync()
     .then(() => app.listen(port, () => console.log("Server connectes, port:", port)))
     .catch(err => console.log(err));
